d50eb25cb005883a3788020c2f34e7cf0e1666fd
Hoang Cong Phan <69571824+hoangcp8700@users.noreply.github.com>
2022-05-30 17:29:15 +0700

    update seo

--- src/components/templates/SEO/index.tsx
+++ src/components/templates/SEO/index.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
 
-type SEOProps = {
-  title: string;
-};
 
-function SEO({ title }: SEOProps) {
+function SEO({ seoData, ogData }: SEOType) {
   // customize meta properties
   // you can pass them as an argument like title in case you want to change for each page
   const description = process.env.siteDescription;
@@ -17,20 +14,40 @@ function SEO({ title }: SEOProps) {
     <Head>
       <meta charSet='utf-8' />
       <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-      <meta name='description' content={description} />
-      <meta name='keywords' content={keywords} />
+      <title>{seoData.title}</title>
+      <meta name='description' content={seoData.description} />
+      <meta name='keywords' content={seoData.keywords} />
+      
       {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' key='twcard' />
-      <meta name='twitter:creator' content={twitterHandle} key='twhandle' />
-
+      <!--  <meta name='twitter:card' content='summary_large_image' key='twcard' />
+      <meta name='twitter:creator' content={twitterHandle} key='twhandle' /> -->
+      <meta name="twitter:title" content={seoData.title} />
+      <meta name="twitter:description" content={seoData?.description} />
+      {
+        seoData?.imgSrc && (
+          <meta name="twitter:image" content={seoData?.imgSrc} />
+        )
+      }
+      
       {/* Open Graph */}
-      <meta property='og:url' content={siteURL} key='ogurl' />
-      <meta property='og:image' content={imagePreview} key='ogimage' />
-      <meta property='og:site_name' content={siteURL} key='ogsitename' />
-      <meta property='og:title' content={title} key='ogtitle' />
-      <meta property='og:description' content={description} key='ogdesc' />
-      <title>{title}</title>
-
+      <meta property='og:site_name' content={ogData?.ogUrl || window.location.href} key='ogsitename' />
+      <meta property="og:url" content={ogData?.ogUrl || window.location.href}  key='ogurl' />
+      {
+        ogData?.ogImage && (
+          <meta property="og:image" content={ogData?.ogImage} />
+        )
+      }
+      {
+        ogData?.ogTitle && (
+          <meta property="og:title" content={ogData?.ogTitle} />
+        )
+      }
+      {
+        ogData?.ogDescription && (
+          <meta property="og:description" content={ogData?.ogDescription} />
+        )
+      }
+      
       <link rel='manifest' href='/manifest.json' />
       <link
         href='https://source.unsplash.com/random'

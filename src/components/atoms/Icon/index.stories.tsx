35255f9781ca9a3701a6e3b8f5515676152954bd
hoangcp <hoangcp8700@gmail.com>
2022-05-31 21:03:41 +0700

    fix error

--- src/components/atoms/Icon/index.stories.tsx
+++ src/components/atoms/Icon/index.stories.tsx
@@ -46,3 +46,5 @@
 // export const Default = Template.bind({});
 
 // Default.args = {};
+// TODO: if you want this file, please change branch develop-sb. then run yarn install && yarn storybook and uncomment above
+export default undefined
--- src/components/atoms/Image/index.stories.tsx
+++ src/components/atoms/Image/index.stories.tsx
@@ -70,3 +70,6 @@
 //   blurDataURL:
 //     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==",
 // };
+
+// TODO: if you want this file, please change branch develop-sb. then run yarn install && yarn storybook and uncomment above
+export default undefined

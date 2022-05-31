4abc742f0f46ba2b1c2c896dc02b64626b393d4d
hoangcp <hoangcp8700@gmail.com>
2022-05-31 21:04:29 +0700

    fix error

--- src/containers/Product/index.tsx
+++ src/containers/Product/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import styles from "./product.module.css";
 
-export interface ProductContainerProps {}
+export type ProductContainerProps = {}
 
 const ProductContainer: React.FC<ProductContainerProps> = () => <div>Container Product</div>;
 
